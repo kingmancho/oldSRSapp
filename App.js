64433a6b569ed8c393338f036a8dc4359bd5ffb2
kingmancho <99691054+kingmancho@users.noreply.github.com>
2022-10-26 23:45:10 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,51 +1,48 @@
 import "./App.css";
 
 export default function App() {
-    
-    var jfile = require("./app.json");
-    //set data object
-    var jobj = JSON.stringify(jfile);
-    localStorage.setItem("data", jobj);
+  var jfile = require("./app.json");
+  //set data object
+  var jobj = JSON.stringify(jfile);
+  localStorage.setItem("data", jobj);
 
-    //get data object
-    var getData = localStorage.getItem("data");
-    var data = JSON.parse(getData);
+  //get data object
+  var getData = localStorage.getItem("data");
+  var data = JSON.parse(getData);
 
+  var i1 = 0;
+  function flipCard() {
+    var cardo = document.getElementsByClassName("card");
+    [...cardo].forEach((card) => {
+      card.classList.toggle("is-flipped");
+    });
+  }
 
+  function onFlip() {
+    flipCard();
+    document.getElementById("back").innerHTML = JSON.stringify(
+      data.Cards[i1 + 1].Tran
+    );
+  }
 
-    var i1 = 0;
-    function flipCard() {
-        var cardo = document.getElementsByClassName("card");
-        [...cardo].forEach((card)=> {
-            card.classList.toggle('is-flipped');
-        })
-    }
+  function onResponse() {
+    flipCard();
+    i1 += 2;
+    document.getElementById("front").innerHTML = JSON.stringify(
+      data.Cards[i1].Word
+    );
+  }
 
-   function onFlip() {
-       flipCard();
-       document.getElementById("back").innerHTML = JSON.stringify(data.Cards[i1 + 1].Tran); 
-    }
+  window.onload = function () {
+    document.getElementById("a").style.visibility = "hidden"
 
-    function onResponse() {
-        flipCard();
-        i1+=2;
-        document.getElementById("front").innerHTML = JSON.stringify(data.Cards[i1].Word);
+    document.getElementById("front").innerHTML = JSON.stringify(
+      data.Cards[0].Word
+    );
+ 
+  };
 
-        
-    }
-
-
-
-    window.onload = function() {
-        document.getElementById("front").innerHTML = JSON.stringify(data.Cards[0].Word);
-    }
-
-    
-
-    
-    //JSON.stringify(data.Cards[0].card1[0].Word)
-
-
+  //JSON.stringify(data.Cards[0].card1[0].Word)
 
   /*
     function getCard() {
@@ -53,32 +50,47 @@ export default function App() {
         data.cards[card1[JSON.stringify(data).substring(6,7)].Word];
     }
     
-
     */
 
   return (
-    <div class="scene scene--card">
-      <div class="card">
-        <div class="card__face card__face--front">
-            <div id = "front"></div>
-            <button onClick={onFlip}  id = "showAnswer" class = "showAnswer">Show Answer</button>
-        </div>
+    <div id = "home" class = "home">
+      <div id = "deckTitle">Decks</div>
+      <button class = "sDeck">Spanish</button>
+      <div id = "a" class="scene scene--card">
+        <div class="card a">
+          <div class="card__face card__face--front">
+            <div id="front"></div>
+            <button onClick={onFlip} id="showAnswer" class="showAnswer">
+              Show Answer
+            </button>
+          </div>
 
-        <div class="card__face card__face--back">
-            <div id = "back"></div>
-            <div class ="buttons">
-                <button onClick={onResponse} class = "button">1</button><div class="divider"></div>
-                <button onClick={onResponse} class = "button">2</button><div class="divider"></div>
-                <button onClick={onResponse} class = "button">3</button><div class="divider"></div>
-                <button onClick={onResponse} class = "button">4</button><div class="divider"></div>
-                <button onClick={onResponse} class = "button">5</button>
+          <div class="card__face card__face--back">
+            <div id="back"></div>
+            <div class="buttons a">
+              <button onClick={onResponse} class="button">
+                1
+              </button>
+              <div class="divider"></div>
+              <button onClick={onResponse} class="button">
+                2
+              </button>
+              <div class="divider"></div>
+              <button onClick={onResponse} class="button">
+                3
+              </button>
+              <div class="divider"></div>
+              <button onClick={onResponse} class="button">
+                4
+              </button>
+              <div class="divider"></div>
+              <button onClick={onResponse} class="button">
+                5
+              </button>
             </div>
-            
-
-
+          </div>
         </div>
       </div>
- 
     </div>
   );
 }
